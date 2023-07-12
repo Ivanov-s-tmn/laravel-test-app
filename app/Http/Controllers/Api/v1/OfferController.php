b74e0fa6ff137b3443ef087a15fd61cc670dca9d
Ivanov-s-tmn <ivanov.sappleid@gmail.com>
2023-07-12 11:32:22 +0500

    fix of update endpoint

--- app/Http/Controllers/Api/v1/OfferController.php
+++ app/Http/Controllers/Api/v1/OfferController.php
@@ -25,16 +25,33 @@ class OfferController extends Controller
     {
         $offer = Offer::where('id', $id)->first();
 
-        if(!$offer) {
+        if (!$offer) {
             return response()->json('Offer does not exist', Response::HTTP_BAD_REQUEST);
         }
-
-        $offer->setAttribute('b24_manager_id', $request->get('b24_manager_id'));
-        $offer->setAttribute('manager', $request->get('manager'));
-        $offer->setAttribute('position', $request->get('position'));
-        $offer->setAttribute('avatar', $request->get('avatar'));
-        $offer->setAttribute('status', $request->get('status'));
-        $offer->setAttribute('date_end', $request->get('date_end'));
+        $b24_manager_id = $request->get('b24_manager_id');
+        if ($b24_manager_id) {
+            $offer->setAttribute('b24_manager_id', $b24_manager_id);
+        }
+        $manager = $request->get('manager');
+        if ($manager) {
+            $offer->setAttribute('manager', $manager);
+        }
+        $position = $request->get('position');
+        if ($position) {
+            $offer->setAttribute('position', $position);
+        }
+        $avatar = $request->get('avatar');
+        if ($avatar) {
+            $offer->setAttribute('avatar', $avatar);
+        }
+        $status = $request->get('status');
+        if ($status) {
+            $offer->setAttribute('status', $status);
+        }
+        $date_end = $request->get('date_end');
+        if ($date_end) {
+            $offer->setAttribute('date_end', $date_end);
+        }
 
         $offer->save();
 
